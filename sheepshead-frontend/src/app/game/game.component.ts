28ef22b72740f1000e3201ac686e49bbf5b500eb
Luke Stoltenberg <lukestolt@gmail.com>
2020-02-17 19:29:35 -0600

    added card images and did base example

--- sheepshead-frontend/src/app/game/game.component.ts
+++ sheepshead-frontend/src/app/game/game.component.ts
@@ -35,7 +35,7 @@ export class GameComponent implements OnInit {
     this.app.renderer.resize(window.innerWidth, window.innerHeight);
     this.addToScreen();
 
-    const texture = PIXI.Texture.from('../../assets/characters.png');
+    const texture = PIXI.Texture.from('../../assets/cards/2_of_clubs.png');
     const sprite = new PIXI.Sprite(texture);
     this.app.stage.addChild(sprite);
 
@@ -43,6 +43,36 @@ export class GameComponent implements OnInit {
 
   private addToScreen(): void {
     document.body.appendChild(this.app.view);
+  }
+
+  private renderGame(container: PIXI.Container): void {
+    // const renderer = PIXI.autoDetectRenderer();
+    // renderer.render(container);
+    this.app.stage.addChild(container);
+  }
+
+
+  // this is an example method that is no longer used
+  drawCards(): void {
+    const stage: PIXI.Container = new PIXI.Container();
+    const graphics: PIXI.Graphics = new PIXI.Graphics();
+    const black = 0x000000;
+    const red = 0xff0000;
+    const white = 0xffffff;
+    graphics.beginFill(white);
+    graphics.drawRect(100, 100, 200, 300);
+    graphics.endFill();
+    // card format: card# and right below is the shape
+    const text = new PIXI.Text('5', {fontFamily: 'italic', fontSize: 24, fill: red});
+    text.position.set(110 , 110);
+    graphics.addChild(text);
+    graphics.beginFill(0xff0000);
+    graphics.drawPolygon([100, 100, 110, 110, 100, 120, 90, 110]);
+    graphics.endFill();
+
+    stage.addChild(graphics);
+    this.renderGame(stage);
+
   }
 
 }
