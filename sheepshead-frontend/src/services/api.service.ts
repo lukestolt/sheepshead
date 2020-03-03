ba86f6cf4b9995722a173c2d749f7cead849f62e
Luke Stoltenberg <lukestolt@gmail.com>
2020-03-03 11:09:55 -0600

    WIP: web sockets

--- sheepshead-frontend/src/services/api.service.ts
+++ sheepshead-frontend/src/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { config } from './config'
+import { config } from './server-config'
 import { Observable } from 'rxjs';
 
 
---
+++ sheepshead-frontend/src/services/server-config.ts
@@ -0,0 +1,8 @@
+import { Server } from 'http';
+
+export class ServerConfig {
+    static host: string = 'localhost';
+    static port: string = '8080'; 
+    static serverUrl: string = ServerConfig.host + ':' + Server.port;
+}
+
